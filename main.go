a9cb11fd91959a39b1ad0f3f79b9b4b3a722fedd
thisliu <liuhao25@foxmail.com>
2022-08-07 18:07:04 +0800

    读取请求内容

--- main.go
+++ main.go
@@ -35,7 +35,24 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "创建新的文章")
+	err := r.ParseForm()
+	if err != nil {
+		// 解析错误
+		fmt.Fprint(w, "请提供正确的数据！")
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	body := r.Form.Get("body")
+
+	fmt.Fprintf(w, "r.Form 中 title 的值为: %v <br>", r.FormValue("title"))
+	fmt.Fprintf(w, "r.PostForm 中 title 的值为: %v <br>", r.PostFormValue("title"))
+
+	fmt.Fprintf(w, "r.Form 中 test 的值为: %v <br>", r.FormValue("test"))
+	fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
+
+	fmt.Fprintf(w, "r.PostForm.Get 中 title 的值为: %v <br>", title)
+	fmt.Fprintf(w, "r.Form.Get 中 body 的值为: %v <br>", body)
 }
 
 func forceHtmlMiddleware(next http.Handler) http.Handler {
@@ -63,7 +80,7 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
 		<title>创建文章 —— 我的技术博客</title>
 	</head>
 	<body>
-		<form action="%s" method="post">
+		<form action="%s?test=data" method="post">
 			<p><input type="text" name="title"></p>
 			<p><textarea name="body" cols="30" rows="10"></textarea></p>
 			<p><button type="submit">提交</button></p>
