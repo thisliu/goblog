5a151c17fba7eb05b02a8576a591cd15a61c80cc
thisliu <liuhao25@foxmail.com>
2022-08-09 18:24:24 +0800

    重构中间件

--- main.go
+++ main.go
@@ -1,66 +1,16 @@
 package main
 
 import (
-	"database/sql"
+	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
+	"goblog/pkg/logger"
 	"net/http"
-	"net/url"
-	"strconv"
-	"strings"
-
-	"github.com/gorilla/mux"
 )
-
-var router *mux.Router
-var db *sql.DB
-
-type Article struct {
-	Title, Body string
-	ID          int64
-}
-
-func (article Article) Delete() (rowsAffected int64, err error) {
-	rs, err := db.Exec("DELETE FROM articles WHERE id =" + strconv.FormatInt(article.ID, 10))
-
-	if err != nil {
-		return 0, err
-	}
-
-	if n, _ := rs.RowsAffected(); n > 0 {
-		return n, nil
-	}
-
-	return 0, err
-}
-
-type ArticlesFormData struct {
-	Title, Body string
-	URL         *url.URL
-	Errors      map[string]string
-}
-
-func forceHtmlMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-type", "text/html; charset=utf-8")
-		next.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
 
 func main() {
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
-	router.Use(forceHtmlMiddleware)
-
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+	logger.LogError(err)
 }
