d1a17a8b918d5b739ecc37d744afa825fc56249e
thisliu <liuhao25@foxmail.com>
2022-08-18 23:16:56 +0800

    注册表单验证

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -2,9 +2,12 @@ package controllers
 
 import (
 	"fmt"
-	"goblog/app/models/user"
+	"goblog/pkg/route"
 	view "goblog/resources/views"
 	"net/http"
+
+	"goblog/app/models/user"
+	"goblog/app/requests"
 )
 
 type AuthController struct {
@@ -15,22 +18,32 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
 }
 
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
-	name := r.PostFormValue("name")
-	email := r.PostFormValue("email")
-	password := r.PostFormValue("password")
-
 	_user := user.User{
-		Name:     name,
-		Email:    email,
-		Password: password,
+		Name:            r.PostFormValue("name"),
+		Email:           r.PostFormValue("email"),
+		Password:        r.PostFormValue("password"),
+		PasswordConfirm: r.PostFormValue("password_confirm"),
 	}
 
-	_user.Create()
+	// 2. 表单规则
+	errs := requests.ValidateRegistrationForm(_user)
 
-	if _user.ID > 0 {
-		fmt.Fprint(w, "插入成功，ID 为"+_user.GetStringID())
+	if len(errs) > 0 {
+		// 4.1 有错误发生，打印数据
+		view.RenderSimple(w, view.D{
+			"Errors": errs,
+			"User":   _user,
+		}, "auth.register")
 	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, "创建用户失败，请联系管理员")
+		_user.Create()
+
+		articlesIndexUrl := route.Name2URL("articles.index")
+
+		if _user.ID > 0 {
+			http.Redirect(w, r, articlesIndexUrl, http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "注册失败，请联系管理员")
+		}
 	}
 }
